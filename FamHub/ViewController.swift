60d9c7fa370103b0f8f755167ef0d2a552e45785
mafarja <mfarjalla@gmail.com>
2019-09-06 15:55:40 -0700

    Initial Commit

---
+++ FamHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FamHub
+//
+//  Created by Marcelo Farjalla on 9/6/19.
+//  Copyright © 2019 StackRank, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
